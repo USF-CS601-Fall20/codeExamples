86039b1333167ac6e7f8ec8f06155c9794fa4a3f
Olga Karpenko <okarpenko@usfca.edu>
2020-09-16 09:19:10 -0700

    Added a Singleton example.

--- src/main/java/designPatterns/singleton/Singleton.java
+++ src/main/java/designPatterns/singleton/Singleton.java
@@ -1,5 +1,15 @@
 package designPatterns.singleton;
 
+/** Example of a class that implements the Singleton pattern */
 public class Singleton {
+  private static  Singleton singleton = new Singleton();
+
+  private Singleton() { // Constructor must be private
+  }
+
+  public static Singleton getInstance() {
+      return singleton;
+  }
+
 
 }
---
+++ src/main/java/designPatterns/singleton/SingletonExample.java
@@ -0,0 +1,10 @@
+package designPatterns.singleton;
+
+public class SingletonExample {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+
+    }
+}
