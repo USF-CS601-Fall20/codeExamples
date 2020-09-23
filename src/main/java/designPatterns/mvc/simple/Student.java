af1f2b841016143d2b2b38eced86a94b8f3f0a69
Olga Karpenko <okarpenko@usfca.edu>
2020-09-23 10:15:06 -0700

    Added MVC examples.

---
+++ src/main/java/designPatterns/mvc/simple/Student.java
@@ -0,0 +1,33 @@
+package designPatterns.mvc.simple;
+
+public class Student {
+    private String name;
+    private double GPA;
+    private int id;
+
+    public Student(String name, double GPA, int id) {
+        this.name = name;
+        this.GPA = GPA;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name  + ", " + id + ", " + GPA;
+    }
+}
