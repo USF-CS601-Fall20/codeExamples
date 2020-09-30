5ca39a4e67d3993d724ff67cdb2a3a50ac4fb560
Olga Karpenko <okarpenko@usfca.edu>
2020-09-30 02:26:23 -0700

    Multithreading examples: volatile.

---
+++ src/main/java/concurrency/volatileKeyword/StopThread.java
@@ -0,0 +1,29 @@
+package concurrency.volatileKeyword;
+
+import java.util.concurrent.TimeUnit;
+
+/** Example from Effective Java book. The program does not stop.
+ * The issue is with visibility. */
+public class StopThread {
+    private static boolean stopRequested = false;
+
+    public static void main (String[] args) {
+
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (!stopRequested) {
+                 i++;
+            } // while
+        }); //Thread
+
+        backgroundThread.start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Thread was interrupted.");
+        }
+
+        stopRequested = true;
+    }
+}
