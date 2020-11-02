020ed29f5c66faea0ec051319723a52c2b56bcd4
Olga Karpenko <okarpenko@usfca.edu>
2020-11-02 10:12:49 -0800

    Examples on HTML form processing.

--- src/main/java/servlets/HelloServlet.java
+++ src/main/java/servlets/HelloServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import org.apache.commons.text.StringEscapeUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,16 +32,17 @@ public class HelloServlet extends HttpServlet {
 		if (name == null || name.isEmpty())
 			name = "anonymous";
 
-		System.out.println(name);
+		//System.out.println(name);
 		name = StringEscapeUtils.escapeHtml4(name); // need to "clean up" whatever
 													// the user entered
-		System.out.println(name);
+		//System.out.println(name);
 		// writing to the response
 		out.println("<html>");
 		out.println("<body>");
 		out.println("<h1>Hello, " + name + "!</h1>");
 		out.println("</body>");
 		out.println("</html>");
+
 
 		// Referencing Prof. Engle
 		// If we did not call escapeHtml4,
@@ -49,7 +51,7 @@ public class HelloServlet extends HttpServlet {
 
 		// You can comment out this line: name = StringEscapeUtils.escapeHtml4(name);
 		// and uncomment the line below to disable security feature in the browser
-		//response.setIntHeader("X-XSS-Protection", 0);
+		 response.setIntHeader("X-XSS-Protection", 0);
 		// Then in the browser, try specifying the text below instead of the name:
 		// ?name=<script>window.open("http://www.usfca.edu/");</script>
 		// Did you see what happened? This script opened another page with usfca.edu website!
